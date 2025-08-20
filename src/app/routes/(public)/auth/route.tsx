febb98bcbbb5e0fd7dc2c32d03224b1cb647246f
Vunky Himawan <tingkerbeii44@gmail.com>
2025-08-20 09:38:05 +0700

    fix: update pre-commit hook permissions and structure; remove unused auth route

--- src/app/routes/(public)/auth/route.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/(public)/auth")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <Outlet />;
-}

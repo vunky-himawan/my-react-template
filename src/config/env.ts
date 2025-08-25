52a615b601f111b2405f6a1ea623ff23df29924c
Vunky Himawan <tingkerbeii44@gmail.com>
2025-08-26 00:23:43 +0700

    fix: add .env example and adjust config file

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,10 @@ export const tanstackQueryConfig = {
     : 300000, // Default to 5 minutes
   retry: import.meta.env.VITE_TANSTACK_QUERY_MAX_RETRIES
     ? parseInt(import.meta.env.VITE_TANSTACK_QUERY_MAX_RETRIES)
-    : 3, // Default to 3 retries
+    : 3, // Default to 3 retries,
+  gcTime: import.meta.env.VITE_TANSTACK_QUERY_GC_TIME
+    ? parseInt(import.meta.env.VITE_TANSTACK_QUERY_GC_TIME)
+    : 60000, // Default to 60 seconds
 };
 
-export const apiUrl = import.meta.env.VITE_API_URL;
+export const apiUrl = import.meta.env.VITE_API_BASE_URL;
--- src/shared/lib/axios/index.ts
+++ src/shared/lib/axios/index.ts
@@ -1,5 +1,6 @@
+import { apiUrl } from "@/config/env";
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
+  baseURL: apiUrl || "http://localhost:3000/api",
 });

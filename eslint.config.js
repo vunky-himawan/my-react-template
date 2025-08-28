b01bbac468b153fde984aedc5d6545e8c4bec7b7
Vunky Himawan <tingkerbeii44@gmail.com>
2025-08-28 16:04:13 +0700

    Implement code changes to enhance functionality and improve performance

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,7 @@ export default tseslint.config([
       globals: globals.browser,
     },
     rules: {
-      "react-refresh/only-export-components": "warn",
+      "react-refresh/only-export-components": "off",
     },
   },
 ]);
